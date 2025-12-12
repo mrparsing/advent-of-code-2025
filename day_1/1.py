666e951cb0e0aa2b5ab8f65fb1ec96367d112619
mrparsing <fralindi5@gmail.com>
2025-12-12 21:40:59 +0100

    day 1 and 2

---
+++ day_1/1.py
@@ -0,0 +1,18 @@
+counter = 0
+sum = 50
+
+with open("input.txt", "r") as file:
+    for row in file:
+        row = row.strip()
+        direction = row[0]
+        value = int(row[1:])
+
+        if direction == "L":
+            sum = (sum - value) % 100
+        else:
+            sum = (sum + value) % 100
+
+        if sum == 0:
+            counter += 1
+
+print(counter)
---
+++ day_2/1.py
@@ -0,0 +1,16 @@
+with open("input.txt", "r") as file:
+    for id in file:
+        id = id.split(",")
+
+sum = 0 
+for i in id:
+    first_id, second_id = i.split("-")
+    for x in range(int(first_id), int(second_id)+1):
+        number = str(x)
+        len_numb = len(number)
+        if len_numb % 2 == 0:
+            first_part = number[0:len_numb//2]
+            second_part = number[len_numb//2:len_numb]
+            if first_part == second_part:
+                sum += int(number)
+print(sum)
